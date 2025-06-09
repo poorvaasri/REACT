e5d49b6c9dde1b605c80a23e0a6bf59548e1f881
Pooja Sri <poorvaasri1026@gmail.com>
2025-06-09 14:06:37 +0530

    added backend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <h1>Vanakkoo!!</h1>
+    </div>
+  );
+}
+
+export default App;
